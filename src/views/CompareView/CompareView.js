1ace72cc0bd26fdd902dc865cb5f030a17bd6b09
Tal Yaron <tal.yaron@gmail.com>
2019-01-04 11:58:48 +0200

    we have votes

--- src/views/CompareView/CompareView.js
+++ src/views/CompareView/CompareView.js
@@ -14,23 +14,28 @@ const CompareView = {
 
     },
     view: function (vnode) {
-        return (
-            <div class='compareImgsWrapper'>
-                <div class='compareImgDiv' onclick={() => setImageSelection(0)}>
-                    <img class='compareImg' src={store.options[0].img}></img>
-                    <div class='compareButton'>מי יפה יותר?</div>
+        if (store.options.length > 0) {
+            return (
+                <div class='compareImgsWrapper'>
+                    <div class='compareImgDiv' onclick={() => setImageSelection(0, vnode)}>
+                        <img class='compareImg' src={store.options[0].img}></img>
+                        <div class='compareButton'>מי יפה יותר?</div>
+                    </div>
+                    <div class='compareImgDiv' onclick={() => setImageSelection(1, vnode)}>
+                        <img class='compareImg' src={store.options[1].img}></img>
+                        <div class='compareButton'>מי יפה יותר?</div>
+                    </div>
                 </div>
-                <div class='compareImgDiv' onclick={() => setImageSelection(1)}>
-                    <img class='compareImg' src={store.options[1].img}></img>
-                    <div class='compareButton'>מי יפה יותר?</div>
-                </div>
-            </div>
-        )
+            )
+        } else {
+            return (<div></div>)
+        }
     }
 }
 
-function setImageSelection(imageSelection) {
-    DB.child('votes/' + store.askingUser + '/' + vnode.attrs.id + '/votes/' + store.user.uid).update({ option: imageSelection })
+function setImageSelection(imageSelection, vnode) {
+
+    DB.child('votersVote/' + store.askingUser + '/' + vnode.attrs.id + '/votes/' + store.user.uid).update({ option: imageSelection })
 }
 
 // functions
